27f5bcbebed46310e3c69355860821e46b24d37a
jeison <jeison07777@gmail.com>
2022-05-06 15:33:28 -0500

    inicializando variables

--- app/src/main/java/com/example/mybarberssgold/MainActivity.kt
+++ app/src/main/java/com/example/mybarberssgold/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.mybarberssgold
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+    var EditText = name,Pwd
+    val button = login
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         name =(EditText)findViewById(R.id.Pwd)
+
+         button =(button)findViewById(R.id.login)
     }
+
 }
