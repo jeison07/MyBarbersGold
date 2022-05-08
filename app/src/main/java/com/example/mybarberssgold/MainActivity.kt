c97f127ff437388f8438e2cdf096c124764780d6
jeison <jeison07777@gmail.com>
2022-05-08 15:24:33 -0500

    programando la logica entre activity_main.xml a activity_main2.xml y viseversa

--- app/src/main/java/com/example/mybarberssgold/MainActivity.kt
+++ app/src/main/java/com/example/mybarberssgold/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.example.mybarberssgold
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
-class MainActivity : AppCompatActivity()  {
-    var EditText = name,Pwd
-    val button = login
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-         name =(EditText)findViewById(R.id.Pwd)
 
-         button =(button)findViewById(R.id.login)
+    class MainActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+
+            val registrarse = findViewById<TextView>(R.id.registra)
+            val cajaEmail = findViewById<EditText>(R.id.name)
+            val cajaPass = findViewById<EditText>(R.id.textPwd)
+            val bntsecion = findViewById<Button>(R.id.login)
+            val url = "http://localhost/login/sencion.php"
+            val queue = Volley.newRequestQueue(this)
+
+
+            registrarse.setOnClickListener() {
+                val ventanaRegistro = Intent(this, MainActivity2::class.java)
+                startActivity(ventanaRegistro)
+            }
+        }
+
+
     }
 
-}
+
+
+
+
+
--- app/src/main/java/com/example/mybarberssgold/MainActivity2.kt
+++ app/src/main/java/com/example/mybarberssgold/MainActivity2.kt
@@ -1,11 +1,22 @@
 package com.example.mybarberssgold
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
+        val texusuario = findViewById<TextView>(R.id.textIniciarSesion)
+        texusuario.setOnClickListener() {
+            val ventanainiciarSecion = Intent(this, MainActivity::class.java)
+            startActivity(ventanainiciarSecion)
+        }
     }
+
+
 }
